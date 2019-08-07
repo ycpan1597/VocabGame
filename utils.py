0b584e02e0676a21a3508be9f330c4253322de89
ycpan1597 <ycpan@uw.edu>
2019-08-07 21:59:13 +0800

    added 17.csv (incomplete)

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import random
 def countScores(num, bank, shuffle = True, maxTries = 3):
     totalPoints = 0
+    wrong = []
     keys = list(bank.keys())
     if num > len(keys):
         num = len(keys)
@@ -15,7 +16,12 @@ def countScores(num, bank, shuffle = True, maxTries = 3):
                 response = input('please try again ({} tries left)'.format(maxTries - numTries)).strip()
                 if numTries == 3:
                     print('you\'ve tried three times; the answer is {}'.format(bank[key]))
+                    wrong.append((key, bank[key]))
                     totalPoints -= 1
                     break
             totalPoints += 1
     print('Score = %.2f%%' % (totalPoints * 100/ num))
+    print('You got these wrong: ')
+    if len(wrong) > 0:
+        for item in wrong:
+            print(item[0], item[1])
